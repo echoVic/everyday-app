0091d9aad619933f42f135a659b1ed313f2fcc32
echovic <137844255@qq.com>
2018-11-22 14:51:35 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {List,Avatar} from "antd";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+  componentWillMount() {
+    axios
+      .get("/api/toutiao/index?type=top&key=786045028d7947db48cc4b0c12599889")
+      .then(res => {
+        console.log(res.data.result);
+        this.setState({
+          news: res.data.result.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
+    const news = this.state.news;
+    const newsItems = news.map(item => (
+      <li key={item.uniquekey}>
+        <a href={item.url}> {item.title} </a> <span> {item.date} </span>{" "}
+        <span> {item.author_name} </span>{" "}
+      </li>
+    ));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <List
+    itemLayout="horizontal"
+    dataSource={news}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar size="large" shape="square" src={item.thumbnail_pic_s}/>}
+          title={item.author_name}
+          description={<a href={item.url}>{item.title}</a>}
+        />
+      </List.Item>
+    )}
+  />
       </div>
     );
   }
